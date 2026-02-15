c240ef52dae9094ceff304e7c6817383e7cf7c5c
Gigster Dev <gigster@example.com>
2026-02-15 09:12:55 +0100

    feat: Add Dockerfile for Next.js application and configure standalone output with remote image patterns.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 's1.ticketm.net' },
+      { protocol: 'https', hostname: 's2.ticketm.net' },
+      { protocol: 'https', hostname: 's3.ticketm.net' },
+      { protocol: 'https', hostname: 's4.ticketm.net' },
+    ],
+  },
 };
 
 export default nextConfig;
